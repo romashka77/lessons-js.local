a3e18c187e7344d1b0710be0af6e0ce67fa935a6
romashka_77 <romashka_77@live.ru>
2022-01-17 12:27:50 +0300

    9.10

---
+++ lesson_9_10.php
@@ -0,0 +1,7 @@
+<?php
+	sleep(1);
+	$x = $_POST['x']? htmlspecialchars($_POST['x']) : 0;
+	$y = $_POST['y']? htmlspecialchars($_POST['y']) : 0;
+	if (!is_numeric($x) || !is_numeric($y)) echo 'Некорректные данные';
+	else echo $x + $y;
+?>
